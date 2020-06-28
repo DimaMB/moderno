4a856c68051efea37a49a5d9d2bf821643f7cdaf
DimaMB <dimamad491@gmail.com>
2020-06-28 13:46:55 +0200

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,14 @@ $(function(){
 
 
 
+  var mixir = mixitup('.products__inner-box')
+
+  $('.product-slider__inner').slick({
+     dots: true,
+     arrows: false,
+     slidesToShow: 4,
+     slidesToScroll: 4,
+
+  }); 
 
 })
